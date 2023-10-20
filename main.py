560936d37a6e1eb2d2707f85a9131f1c3cb3ff2c
михаил масленников <miha.masel@mail.ru>
2023-10-20 11:09:35 +0300

    Dates made good

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 
 from database.server_methods.create import create_compound_products, create_fabric, create_implementation, create_ingredients, create_procurement, create_product
-from datetime import datetime
+from datetime import datetime, timedelta
 from database.server_methods.delete import drop_table
 import random
 
-def start():
 
+# import random
+# from datetime import datetime, timedelta
+
+# # Генерация случайной даты в диапазоне 1 год
+# start_date = datetime.now() - timedelta(days=365)
+# random_date = start_date + timedelta(days=random.randint(0, 365))
+
+# # Прибавление 1 года к случайной дате
+# new_date = random_date + timedelta(days=365)
+
+# # Вывод результатов
+# print(f"Случайная дата: {random_date}")
+# print(f"Дата после добавления 1 года: {new_date}")
+
+def start():
+    start_date = datetime.now() - timedelta(days=14)
+    expiration = start_date + timedelta(days=14)
+    procurement = start_date
     product_name = [
         "Bread",
         "Baggutte",
@@ -22,12 +39,12 @@ def start():
     ]
     for i in range(1000):
         try:
-            product_id = create_product(random.choice(product_name), random.randint(10, 100), datetime.now(), random.randint(10, 100), random.randint(10, 100))
-            create_fabric(product_id, random.randint(10, 100), datetime.now(), random.randint(10, 100))
-            create_implementation(product_id, random.randint(10, 100), datetime.now())
+            product_id = create_product(random.choice(product_name), random.randint(10, 100), expiration, random.randint(10, 100), random.randint(10, 100))
+            create_fabric(product_id, random.randint(10, 100), start_date, random.randint(10, 100))
+            create_implementation(product_id, random.randint(10, 100), start_date)
             compound_id = create_compound_products(product_id, random.randint(10, 100))
-            ingredient_id = create_ingredients(compound_id, datetime.now(), random.randint(10, 100), random.randint(10, 100))
-            create_procurement(ingredient_id, random.randint(10, 100), datetime.now(), True)
+            ingredient_id = create_ingredients(compound_id, expiration, random.randint(10, 100), random.randint(10, 100))
+            create_procurement(ingredient_id, random.randint(10, 100), procurement, True)
 
         except Exception as e:
             print(e)
